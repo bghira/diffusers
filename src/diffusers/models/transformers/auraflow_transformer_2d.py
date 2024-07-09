019ef3e15abfca1dc68ecac3a47d1283833c4d0e
bghira <bghira@users.github.com>
2024-07-09 08:02:39 -0600

    aura: update naming conventions to follow upstream PR

--- src/diffusers/models/transformers/auraflow_transformer_2d.py
+++ src/diffusers/models/transformers/auraflow_transformer_2d.py
@@ -21,9 +21,9 @@ import torch.nn.functional as F
 
 from ...configuration_utils import ConfigMixin, register_to_config
 from ...utils import is_torch_version, logging
+from ...loaders import PeftAdapterMixin, FromOriginalModelMixin
 from ...utils.torch_utils import maybe_allow_in_graph
 from ..attention_processor import Attention, AuraFlowAttnProcessor2_0
-from ...loaders import FromOriginalModelMixin, PeftAdapterMixin
 from ..embeddings import TimestepEmbedding, Timesteps
 from ..modeling_outputs import Transformer2DModelOutput
 from ..modeling_utils import ModelMixin
@@ -87,16 +87,16 @@ class AuraFlowFeedForward(nn.Module):
         if hidden_dim is None:
             hidden_dim = 4 * dim
 
-        n_hidden = int(2 * hidden_dim / 3)
-        n_hidden = find_multiple(n_hidden, 256)
+        final_hidden_dim = int(2 * hidden_dim / 3)
+        final_hidden_dim = find_multiple(final_hidden_dim, 256)
 
-        self.c_fc1 = nn.Linear(dim, n_hidden, bias=False)
-        self.c_fc2 = nn.Linear(dim, n_hidden, bias=False)
-        self.c_proj = nn.Linear(n_hidden, dim, bias=False)
+        self.linear_1 = nn.Linear(dim, final_hidden_dim, bias=False)
+        self.linear_2 = nn.Linear(dim, final_hidden_dim, bias=False)
+        self.out_projection = nn.Linear(final_hidden_dim, dim, bias=False)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = F.silu(self.c_fc1(x)) * self.c_fc2(x)
-        x = self.c_proj(x)
+        x = F.silu(self.linear_1(x)) * self.linear_2(x)
+        x = self.out_projection(x)
         return x
 
 
@@ -160,10 +160,8 @@ class AuraFlowTransformerBlock(nn.Module):
         is_last (`bool`): Boolean to determine if this is the last block in the model.
     """
 
-    def __init__(self, dim, num_attention_heads, attention_head_dim, is_last=False):
+    def __init__(self, dim, num_attention_heads, attention_head_dim, context_norm_type="ada_norm_zero"):
         super().__init__()
-        self.is_last = is_last
-        context_norm_type = "ada_norm_continous" if is_last else "ada_norm_zero"
 
         self.norm1 = AdaLayerNormZero(dim, bias=False, norm_type="fp32_layer_norm")
 
@@ -173,6 +171,10 @@ class AuraFlowTransformerBlock(nn.Module):
             )
         elif context_norm_type == "ada_norm_zero":
             self.norm1_context = AdaLayerNormZero(dim, bias=False, norm_type="fp32_layer_norm")
+        else:
+            raise ValueError(
+                "Invalid norm type provided for `context_norm_type`. Valid values are are: 'ada_norm_continous' and 'ada_norm_zero'."
+            )
 
         processor = AuraFlowAttnProcessor2_0()
         self.attn = Attention(
@@ -193,10 +195,7 @@ class AuraFlowTransformerBlock(nn.Module):
         self.norm2 = FP32LayerNorm(dim, elementwise_affine=False, bias=False)
         self.ff = AuraFlowFeedForward(dim, dim * 4)
         self.norm2_context = FP32LayerNorm(dim, elementwise_affine=False, bias=False)
-        if not is_last:
-            self.ff_context = AuraFlowFeedForward(dim, dim * 4)
-        else:
-            self.ff_context = None
+        self.ff_context = AuraFlowFeedForward(dim, dim * 4)
 
     def forward(
         self, hidden_states: torch.FloatTensor, encoder_hidden_states: torch.FloatTensor, temb: torch.FloatTensor, i=0
